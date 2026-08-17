700c883208862896938a0f1fd1a9a7761f8c449b
siddharthavarma-lgtm <siddharthavarma@klh.edu.in>
2026-08-17 11:12:23 +0530

    Create Q1.code.c

---
+++ SKILL/Q1.code.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+    char cmd[50];
+    pid_t pid;
+
+    printf("Enter Linux Command: ");
+    scanf("%s", cmd);
+
+    pid = fork();
+
+    if (pid < 0)
+    {
+        printf("Fork Failed\n");
+        return 1;
+    }
+
+    else if (pid == 0)
+    {
+        printf("\nChild Process\n");
+        printf("Child PID : %d\n", getpid());
+        printf("Parent PID: %d\n", getppid());
+
+        execlp(cmd, cmd, NULL);
+
+        perror("Command Execution Failed");
+        exit(1);
+    }
+
+    else
+    {
+        printf("\nParent Process\n");
+        printf("Parent PID : %d\n", getpid());
+        printf("Child PID  : %d\n", pid);
+
+        wait(NULL);
+
+        printf("\nChild Process Completed\n");
+    }
+
+    return 0;
+}
